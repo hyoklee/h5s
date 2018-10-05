325ae3337470282515fe4bf280c34f3304498389
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2018-10-04 23:39:39 -0500

    added zf_log binding.

--- src/H5Flog.c
+++ src/H5Flog.c
@@ -1,7 +1,19 @@
-void H5F_post_vfd_swrm_log_entry(H5F_file_t f, int entry_type_code, char * body)
+#include <zf_log.h>
+#include <hdf5.h>
+
+void 
+H5F_post_vfd_swrm_log_entry(hid_t fid, int entry_type_code, char * body)
 {
-  
+  if (fid < 0) 
+    return; 			/* No operation */
+
+  ZF_LOGI("%i %s", entry_type_code, body);
 }
 
+/* Test. */
+int main(int argc, char** argv)
+{
+  H5F_post_vfd_swrm_log_entry(1, 1, "test");
+}
 
 
