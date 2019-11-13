80dc32c2dae96f465a033402250b554c94d37d81
H. Joe Lee <hyoklee@hdfgroup.org>
2019-11-13 13:04:25 -0600

    added more fields to Elastic index. added drop statement for MongoDB.

--- bin/mongoimport.sh
+++ bin/mongoimport.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Drop DB.
+mongo --eval "db.ior.drop()"
+mongo --eval "db.output.drop()"
 
 # Go over IOR json files and import them to MongoDB.
-for file in ../data/ior/*.json
+for file in ../data/ior/*ior.*
 do
   echo $file
   mongoimport -d test -c ior $file
--- test/beam_ior_mongo.py
+++ test/beam_ior_mongo.py
@@ -6,13 +6,40 @@ outputs_prefix = 'outputs/part'
 
 def transform_doc(document):
   out = {}
+
+
+  # Handle timestamp.
   dt = str(document['Began'])
+  
   # Cut first 4 characters.
   v_dt = dt[4:]
   dto = datetime.datetime.strptime(v_dt, '%b %d %H:%M:%S %Y')
   s_e = dto.strftime('%Y-%m-%dT%H:%M:%SZ')
   out['ts'] = s_e
-  out['value'] = document['summary'][0]['MeanTime']
+
+  # Handle API and version.
+  api = str(document['tests'][0]['Options']['api'])
+  apv = str(document['tests'][0]['Options']['apiVersion'])
+  out['api'] = api
+  out['apv'] = apv
+
+  # Handle clients per node, tasks, and aggregate file size.
+  cpn = document['tests'][0]['Options']['clients per node']
+  tsk = document['tests'][0]['Options']['tasks']
+  agf = document['tests'][0]['Options']['aggregate filesize']
+  
+  out['cpn'] = cpn
+  out['tsk'] = tsk
+  out['agf'] = agf
+
+  # Handle metric values.
+  out['wmt'] = document['summary'][0]['MeanTime']
+  out['wbw'] = document['summary'][0]['bwMeanMIB']
+  out['rmt'] = document['summary'][1]['MeanTime']
+  out['rbw'] = document['summary'][1]['bwMeanMIB']
+
+  # TO-DO: tasksPerNode?
+
   return out
   # For text output.
   # return json.dumps(out) 
