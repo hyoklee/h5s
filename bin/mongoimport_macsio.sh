ac9694e5ecf019e779795d3e52c24bfebb6d39bb
H. Joe Lee <hyoklee@hdfgroup.org>
2020-04-06 13:06:35 -0500

    TRILAB-177:changed DB name for MACSio.

--- bin/mongoimport_macsio.sh
+++ bin/mongoimport_macsio.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Drop DB.
-mongo --eval "db.macsioinputs.drop()"
-# mongo --eval "db.macsiooutputs.drop()"
+
+# List all items in the input table.
+# mongo --eval "db.macsioinputs.find()" macsio
+# Drop input table.
+mongo --eval "db.macsioinputs.drop()" macsio
+
+# List all items in the output table.
+# mongo --eval "db.macsiooutputs.find()" macsio
+# Drop output table.
+# mongo --eval "db.macsiooutputs.drop() macsio"
+
 
 # Go over IOR json files and import them to MongoDB.
 for file in ../data/macsio/*.json
 do
   echo $file
-  mongoimport -d test -c macsioinputs $file
+  mongoimport -d macsio -c macsioinputs $file
 done
