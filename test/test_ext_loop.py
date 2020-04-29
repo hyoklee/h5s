a6d6a2221196e7595df0e781830e8c8efe532e8f
H. Joe Lee <hyoklee@hdfgroup.org>
2020-04-29 15:17:04 -0500

    HDFFV-9984:updated loop testing.

--- test/test_ext_loop.py
+++ test/test_ext_loop.py
@@ -1,16 +1,13 @@
 import h5py
 import numpy as np
 
-c = h5py.File('c.h5', 'w')
-arr_c = np.arange(10)
-dset_c = c.create_dataset("dset", data=arr_c)
-b = h5py.File('b.h5', 'w')
-arr = np.arange(100)
-dset = b.create_dataset("dset", data=arr)
-b['ext link'] = h5py.ExternalLink("c.h5", "/dset")
 a = h5py.File('a.h5','w')
 arr_a = np.arange(5)
 dset_a = a.create_dataset("dset", data=arr_a)
-a['ext link'] = h5py.ExternalLink("b.h5", "/dset")
+c = h5py.File('c.h5', 'w')
 c['ext link'] = h5py.ExternalLink("a.h5", "/dset")
+b = h5py.File('b.h5', 'w')
+b['ext link'] = h5py.ExternalLink("c.h5", "/ext link")
+a['ext link'] = h5py.ExternalLink("b.h5", "/ext link")
+
 
